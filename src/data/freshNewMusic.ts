ac334756ab0136a2b528466617608762c85dda7b
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-05-13 19:31:13 +0545

    added the fresh new music cards

---
+++ src/data/freshNewMusic.ts
@@ -0,0 +1,41 @@
+import TFreshNewMusic from "@/type/TFreshNewMusic";
+
+const freshNewMusicContent: TFreshNewMusic[] = [
+  {
+    _id: 0,
+    image: "/freshNewMusic/popRising.png",
+    title: "Pop Rising",
+    description: "Who's now and next in pop.",
+    isHovering: false,
+  },
+  {
+    _id: 1,
+    image: "/freshNewMusic/animeNow.png",
+    title: "Anime Now",
+    description: "Hottest Anime tracks & best new sounds for all...",
+    isHovering: false,
+  },
+  {
+    _id: 2,
+    image: "/freshNewMusic/newMusicFriday.png",
+    title: "New Music Friday",
+    description: "The very best is new music from around the....",
+    isHovering: false,
+  },
+  {
+    _id: 3,
+    image: "/freshNewMusic/popRightNow.png",
+    title: "Pop Right Now",
+    description: "The most interesting tracks in the current wor...",
+    isHovering: false,
+  },
+  {
+    _id: 4,
+    image: "/freshNewMusic/justHits.png",
+    title: "Just Hits",
+    description: "Currents favourite and exciting new music...",
+    isHovering: false,
+  },
+];
+
+export default freshNewMusicContent;
---
+++ src/type/TFreshNewMusic.ts
@@ -0,0 +1,9 @@
+type TFreshNewMusic = {
+  _id: number;
+  image: string;
+  title: string;
+  description: string;
+  isHovering: boolean;
+};
+
+export default TFreshNewMusic;
