3b5ce16dec111fc7c0b741114df77e4a02853d7c
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-05-13 17:12:20 +0545

    added the throwback cards

--- src/components/HomePage-All/HomePageContainer.tsx
+++ src/components/HomePage-All/HomePageContainer.tsx
@@ -2,6 +2,7 @@ import FeaturedCharts from "./FeaturedCharts";
 import HomePageRecommendation from "./HomePageRecommendation";
 import MoodCardContainer from "./MoodCardContainer";
 import RecentlyPlayed from "./RecentlyPlayed";
+import Throwback from "./Throwback";
 import TodayBiggestHits from "./TodaysBiggestHits";
 import TrySomethingElse from "./TrySomethingElse";
 
@@ -10,6 +11,7 @@ const HomePageContainer = () => {
     <div className="h-full w-full py-0 px-6 flex flex-col gap-8 overflow-y-auto">
       <HomePageRecommendation />
       <RecentlyPlayed />
+      <Throwback />
       <TodayBiggestHits />
       <TrySomethingElse />
       <FeaturedCharts />
---
+++ src/data/throwback.ts
@@ -0,0 +1,41 @@
+import TThrowback from "@/type/TThrowback";
+
+const throwbackContent: TThrowback[] = [
+  {
+    _id: 0,
+    image: "/throwback/rockClassics.png",
+    title: "Rock Classics",
+    description: "Rock legends & epic songs that continue to inspire...",
+    isHovering: false,
+  },
+  {
+    _id: 1,
+    image: "/throwback/allOut2000s.png",
+    title: "All Out 2000s",
+    description: "The biggest songs of the 2000s. Cover: Kelly...",
+    isHovering: false,
+  },
+  {
+    _id: 2,
+    image: "/throwback/allOut2010s.png",
+    title: "All Out 2010s",
+    description: "The biggest songs of the 2010s. Cover: Billie...",
+    isHovering: false,
+  },
+  {
+    _id: 3,
+    image: "/throwback/allOut2020s.png",
+    title: "All Out 2020s",
+    description: "The biggest songs of the 2020s.",
+    isHovering: false,
+  },
+  {
+    _id: 4,
+    image: "/throwback/allOut80s.png",
+    title: "All Out 80s",
+    description: "The biggest songs of the 1980s. Cover: Bonnie...",
+    isHovering: false,
+  },
+];
+
+export default throwbackContent;
