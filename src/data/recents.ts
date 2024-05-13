ab6ffda7a1a0a79cd5ae1f588061224654edd42b
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-05-13 15:27:50 +0545

    updated the url for mapped images

--- src/data/recents.ts
+++ src/data/recents.ts
@@ -3,7 +3,7 @@ import TRecent from "@/type/TRecent";
 const recentContent: TRecent[] = [
   {
     _id: 0,
-    image: "/theWeekndMix.png",
+    image: "/recent/theWeekndMix.png",
     title: "The Weeknd Mix",
     featurings: "Chris Brown, Miguel and Chase Atlantic",
     isHovering: false,
