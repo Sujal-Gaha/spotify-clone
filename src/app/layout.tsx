efa78c0ab40809bf5cea9795fd6537266895208a
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-05-29 15:28:44 +0545

    fixed the layout for content-changer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 import LeftSideBarContainer from "@/components/LeftSideBar/LeftSideBarContainer";
 import RightSideBarContainer from "@/components/RightSideBar/RightSideBarContainer";
 import useRightSideBarStore from "@/store/RightSideBarStore";
+import HomePageContentChanger from "@/components/HomePageContentChanger";
+import clsx from "clsx";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
   children,
@@ -18,7 +21,16 @@ export default function RootLayout({
       <body className="p-2 h-screen flex flex-col gap-2">
         <div className="flex gap-2">
           <LeftSideBarContainer />
-          {children}
+          <div
+            className={clsx(
+              "h-[90vh] bg-sectionColour rounded-lg flex flex-col items-center gap-2",
+              isRightSideBarVisible ? "w-[1048px]" : "w-full"
+            )}
+          >
+            <Navbar />
+            <HomePageContentChanger />
+            {children}
+          </div>
           {isRightSideBarVisible && <RightSideBarContainer />}
         </div>
         <AudioControllerContainer />
---
+++ src/app/music/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Music = () => {
+  return <div className="w-full h-screen bg-purple-400">Music</div>;
+};
+
+export default Music;
