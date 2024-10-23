7991084614635ac20297b91da49d642c93acf32e
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-10-23 20:05:38 +0545

    removed unwanted functions and fixed the tailwind config

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,39 +1,12 @@
 "use client";
 
-import { useState } from "react";
 import useHomePageAllContentStore from "@/stores/homePageAllContentStore";
 import Footer from "./Footer";
 import PlaylistBar from "./PlaylistBar";
 import { Card } from "./ui/card";
 
 const All = () => {
-  const { homePageAllContent, setHomePageAllContent } =
-    useHomePageAllContentStore();
-
-  const [containerId, setContainerId] = useState(NaN);
-  const [cardId, setCardId] = useState(NaN);
-
-  const handleMouseOver = () => {
-    const updatedHomePageAllContent = [...homePageAllContent];
-    if (
-      updatedHomePageAllContent[containerId] &&
-      updatedHomePageAllContent[containerId].content[cardId]
-    ) {
-      updatedHomePageAllContent[containerId].content[cardId].isHovering = true;
-      setHomePageAllContent(updatedHomePageAllContent);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    const updatedHomePageAllContent = [...homePageAllContent];
-    if (
-      updatedHomePageAllContent[containerId] &&
-      updatedHomePageAllContent[containerId].content[cardId]
-    ) {
-      updatedHomePageAllContent[containerId].content[cardId].isHovering = false;
-      setHomePageAllContent(updatedHomePageAllContent);
-    }
-  };
+  const { homePageAllContent } = useHomePageAllContentStore();
 
   return (
     <div className="flex flex-col overflow-y-auto px-5 w-full pt-2 no-scrollbar">
@@ -41,18 +14,7 @@ const All = () => {
       <div className="h-full w-full pt-0 pb-6 flex flex-col gap-8 ">
         {homePageAllContent.map((allContent) => {
           return (
-            <div
-              className="flex flex-col gap-2"
-              key={allContent.mainId}
-              onMouseLeave={() => {
-                handleMouseLeave();
-                setContainerId(NaN);
-              }}
-              onMouseOver={() => {
-                handleMouseOver();
-                setContainerId(allContent.mainId);
-              }}
-            >
+            <div key={allContent.mainId} className="flex flex-col gap-2">
               <div className="w-full flex justify-between items-center px-2">
                 <p className="text-2xl text-white font-semibold">
                   {allContent.mainTitle}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
 import type { Config } from "tailwindcss";
 
-const flowbite = require("flowbite-react/tailwind");
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -27,6 +24,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [flowbite.plugin()],
+  plugins: [],
 };
 export default config;
