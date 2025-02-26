ee1ceb1824a2fcf25c27c5dfb2ff26c3923ab36f
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-02-26 22:11:48 +0545

    added the flasks lib for stores

--- libs/compounds/src/right-sidebar.tsx
+++ libs/compounds/src/right-sidebar.tsx
@@ -3,7 +3,26 @@ import Merch from './Merch';
 import Credits from './Credits';
 import ArtistCard from './ArtistCard';
 import RecommendationCard from './RecommendationCard';
-import RecommendationArtist from './RecommendationArtist';
+
+
+const RecommendationArtist = () => {
+     const { closeRightSidebar } = useRightSidebarStore();
+    
+      return (
+        <div className="w-[420px] h-10 px-4 py-0 flex justify-between items-center">
+          <div>
+            <p className="text-white font-semibold">The Weeknd Mix</p>
+          </div>
+          <div className="text-lessFocusColour w-12 flex justify-between text-xl">
+            <BsThreeDots />
+            <IoMdClose
+              className="hover:text-white cursor-pointer duration-500 ease-in-out"
+              onClick={closeRightSidebar}
+            />
+          </div>
+        </div>
+      );
+}
 
 export const RightSidebar = () => {
   return (
---
+++ libs/flasks/src/lib/flasks.tsx
@@ -0,0 +1,9 @@
+export function Flasks() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Flasks!</h1>
+    </div>
+  );
+}
+
+export default Flasks;
