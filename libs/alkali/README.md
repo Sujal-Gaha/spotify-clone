922fa3b3e254c4a9f00475e418ac837abe9f6a28
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-02-26 22:03:01 +0545

    added alkali library for components that are reactive typically with animations

---
+++ libs/alkali/README.md
@@ -0,0 +1,1 @@
+For reactive components
---
+++ libs/alkali/src/lib/alkali.tsx
@@ -0,0 +1,9 @@
+export function Alkali() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Alkali!</h1>
+    </div>
+  );
+}
+
+export default Alkali;
